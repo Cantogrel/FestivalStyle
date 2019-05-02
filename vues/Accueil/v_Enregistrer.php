6d403144fd7aed4b3c276ef5103802539f537261
arthur <afarthuraf@gmail.com>
2019-05-02 19:06:53 +0200

    fix enregistrement

--- vues/Accueil/v_Enregistrer.php
+++ vues/Accueil/v_Enregistrer.php
@@ -1,6 +1,6 @@
 
 <h1>Ajouter un groupe de Folklores : </h1>
-<form id="login" action="index.php?uc=enregistrer" method="POST">
+<form id="login" action="index.php" method="POST">
     <p><span>Nom du groupe : </span><input id="nom" name="nom" type="text" size="20"/></p>
     <p><span>Nom du Responsable : </span><input id="identiteResponsable" name="identiteResponsable" type="text" size="20"/></p>
     <p><span>Adresse Postale : </span><input id="adressePostale" name="adressePostale" type="text" size="20"/></p>
